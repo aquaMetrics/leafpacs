634c5fccd18de03ba98d2dc0f9d26988d96710e5
fozy81 <fozy81@hotmail.com>
2022-01-31 11:43:03 +0000

    refactor: classify and test

--- R/leafpacs-classify.R
+++ R/leafpacs-classify.R
@@ -31,199 +31,99 @@ leafpacs_classify <- function(data) {
   data <- data %>% mutate(NFG_EQR = (.data$N_RFG / .data$REF_NFG))
 
   # Algal EQR
-  data <- data %>% mutate(ALG_EQR = (.data$RFA_PC - 100) / (.data$REF_ALGAE - 100))
+  data <- data %>%
+    mutate(ALG_EQR = (.data$RFA_PC - 100) / (.data$REF_ALGAE - 100))
 
   # Add adjustments to RMNI EQR------------------------------------------------
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] > 1) {
-      data[n, "RMNI_EQR_ADJ"] <- 1
-    }
-  }
+  data$RMNI_EQR_ADJ[data$RMNI_EQR >= 0.85] <-
+    ((data$RMNI_EQR[data$RMNI_EQR >= 0.85]
+    - 0.85) / (1 - 0.85)) * 0.2 + 0.8
 
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] >= 0.85 & data$RMNI_EQR[n] < 1) {
-      data[n, "RMNI_EQR_ADJ"] <-
-        (((data$RMNI_EQR[n] - 0.85) / (1 - 0.85)) * 0.2 + 0.8)
-    }
-  }
+  data$RMNI_EQR_ADJ[data$RMNI_EQR < 0.85] <-
+    ((data$RMNI_EQR[data$RMNI_EQR < 0.85]
+    - 0.7) / (0.85 - 0.7)) * 0.2 + 0.6
 
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] >= 0.70 & data$RMNI_EQR[n] < 0.85) {
-      data[n, "RMNI_EQR_ADJ"] <-
-        (((data$RMNI_EQR[n] - 0.7) / (0.85 - 0.7)) * 0.2 + 0.6)
-    }
-  }
+  data$RMNI_EQR_ADJ[data$RMNI_EQR < 0.7] <-
+    ((data$RMNI_EQR[data$RMNI_EQR < 0.7]
+    - 0.52) / (0.7 - 0.52)) * 0.2 + 0.4
 
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] >= 0.52 & data$RMNI_EQR[n] < 0.7) {
-      data[n, "RMNI_EQR_ADJ"] <-
-        (((data$RMNI_EQR[n] - 0.52) / (0.7 - 0.52)) * 0.2 + 0.4)
-    }
-  }
+  data$RMNI_EQR_ADJ[data$RMNI_EQR < 0.52] <-
+    ((data$RMNI_EQR[data$RMNI_EQR < 0.52]
+    - 0.34) / (0.52 - 0.34)) * 0.2 + 0.2
 
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] >= 0.34 & data$RMNI_EQR[n] < 0.52) {
-      data[n, "RMNI_EQR_ADJ"] <-
-        (((data$RMNI_EQR[n] - 0.34) / (0.52 - 0.34)) * 0.2 + 0.2)
-    }
-  }
-
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] >= 0.16 & data$RMNI_EQR[n] < 0.34) {
-      data[n, "RMNI_EQR_ADJ"] <-
-        (((data$RMNI_EQR[n] - 0.16) / (0.34 - 0.16)) * 0.2)
-    }
-  }
-
-  for (n in 1:nrow(data)) {
-    if (data$RMNI_EQR[n] < 0.16) {
-      data[n, "RMNI_EQR_ADJ"] <- 0
-    }
-  }
+  data$RMNI_EQR_ADJ[data$RMNI_EQR < 0.34] <-
+    ((data$RMNI_EQR_ADJ[data$RMNI_EQR < 0.34]
+    - 0.16) / (0.34 - 0.16)) * 0.2
 
   # Adjustments to Ntaxa or NFG EQR -------------------------------------------
-  for (n in 1:nrow(data)) {
-    if (min(data$NTAXA_EQR[n], data$NFG_EQR[n]) >= 0.83) {
-      data[n, "Diversity_EQR_ADJ"] <-
-        (((min(
-          data$NTAXA_EQR[n],
-          data$NFG_EQR[n]
-        ) - 0.83) / (1 - 0.83)) * 0.2 + 0.8)
-    }
-  }
+  data$MIN_EQR <- min(data$NTAXA_EQR, data$NFG_EQR)
 
-  for (n in 1:nrow(data)) {
-    if (min(data$NTAXA_EQR[n], data$NFG_EQR[n]) >= 0.66 &
-      min(data$NTAXA_EQR[n], data$NFG_EQR[n]) < 0.83) {
-      data[n, "Diversity_EQR_ADJ"] <-
-        (((min(
-          data$NTAXA_EQR[n],
-          data$NFG_EQR[n]
-        ) - 0.66) / (0.83 - 0.66)) * 0.2 + 0.6)
-    }
-  }
+  data$Diversity_EQR_ADJ[data$MIN_EQR >= 0.83] <-
+    ((data$MIN_EQR[data$MIN_EQR >= 0.83]
+    - 0.83) / (1 - 0.83)) * 0.2 + 0.8
 
-  for (n in 1:nrow(data)) {
-    if (min(data$NTAXA_EQR[n], data$NFG_EQR[n]) >= 0.49 &
-      min(data$NTAXA_EQR[n], data$NFG_EQR[n]) < 0.66) {
-      data[n, "Diversity_EQR_ADJ"] <-
-        (((min(
-          data$NTAXA_EQR[n],
-          data$NFG_EQR[n]
-        ) - 0.49) / (0.66 - 0.49)) * 0.2 + 0.4)
-    }
-  }
+  data$Diversity_EQR_ADJ[data$MIN_EQR < 0.83] <-
+    ((data$MIN_EQR[data$MIN_EQR < 0.83]
+    - 0.66) / (0.83 - 0.66)) * 0.2 + 0.6
 
-  for (n in 1:nrow(data)) {
-    if (min(data$NTAXA_EQR[n], data$NFG_EQR[n]) >= 0.32 &
-      min(data$NTAXA_EQR[n], data$NFG_EQR[n]) < 0.49) {
-      data[n, "Diversity_EQR_ADJ"] <-
-        (((min(
-          data$NTAXA_EQR[n],
-          data$NFG_EQR[n]
-        ) - 0.32) / (0.49 - 0.32)) * 0.2 + 0.2)
-    }
-  }
+  data$Diversity_EQR_ADJ[data$MIN_EQR < 0.66] <-
+    ((data$MIN_EQR[data$MIN_EQR < 0.66]
+    - 0.49) / (0.66 - 0.49)) * 0.2 + 0.4
 
-  for (n in 1:nrow(data)) {
-    if (min(data$NTAXA_EQR[n], data$NFG_EQR[n]) < 0.32) {
-      data[n, "Diversity_EQR_ADJ"] <-
-        (((min(
-          data$NTAXA_EQR[n],
-          data$NFG_EQR[n]
-        ) - 0.15) / (0.32 - 0.15)) * 0.2)
-    }
-  }
+  data$Diversity_EQR_ADJ[data$MIN_EQR < 0.49] <-
+    ((data$MIN_EQR[data$MIN_EQR < 0.49]
+    - 0.32) / (0.49 - 0.32)) * 0.2 + 0.2
+
+  data$Diversity_EQR_ADJ[data$MIN_EQR < 0.32] <-
+    ((data$MIN_EQR[data$MIN_EQR < 0.32]
+    - 0.15) / (0.32 - 0.15)) * 0.2
 
   # Adjustments to ALGAL EQR ---------------------------------------------------
+  data$ALG_EQR_ADJ[data$ALG_EQR >= 0.975] <-
+    ((data$ALG_EQR[data$ALG_EQR >= 0.975]
+    - 0.975) / (1 - 0.975)) * 0.2 + 0.8
 
-  for (n in 1:nrow(data)) {
-    if (data$ALG_EQR[n] >= 0.975) {
-      data[n, "ALG_EQR_ADJ"] <-
-        (((data$ALG_EQR[n] - 0.975) / (1 - 0.975)) * 0.2 + 0.8)
-    }
-  }
+  data$ALG_EQR_ADJ[data$ALG_EQR < 0.975] <-
+    ((data$ALG_EQR[data$ALG_EQR < 0.975]
+    - 0.925) / (0.975 - 0.925)) * 0.2 + 0.6
 
-  for (n in 1:nrow(data)) {
-    if (data$ALG_EQR[n] >= 0.925 & data$ALG_EQR[n] < 0.975) {
-      data[n, "ALG_EQR_ADJ"] <-
-        (((data$ALG_EQR[n] - 0.925) / (0.975 - 0.925)) * 0.2 + 0.6)
-    }
-  }
+  data$ALG_EQR_ADJ[data$ALG_EQR < 0.925] <-
+    ((data$ALG_EQR[data$ALG_EQR < 0.925]
+    - 0.825) / (0.925 - 0.825)) * 0.2 + 0.4
 
-  for (n in 1:nrow(data)) {
-    if (data$ALG_EQR[n] >= 0.825 & data$ALG_EQR[n] < 0.925) {
-      data[n, "ALG_EQR_ADJ"] <-
-        (((data$ALG_EQR[n] - 0.825) / (0.925 - 0.825)) * 0.2 + 0.4)
-    }
-  }
+  data$ALG_EQR_ADJ[data$ALG_EQR < 0.825] <-
+    ((data$ALG_EQR[data$ALG_EQR < 0.825]
+    - 0.625) / (0.825 - 0.625)) * 0.2 + 0.2
 
-  for (n in 1:nrow(data)) {
-    if (data$ALG_EQR[n] >= 0.625 &
-      data$ALG_EQR[n] < 0.825) {
-      data[n, "ALG_EQR_ADJ"] <-
-        (((data$ALG_EQR[n] - 0.625) / (0.825 - 0.625)) * 0.2 + 0.2)
-    }
-  }
-
-  for (n in 1:nrow(data)) {
-    if (data$ALG_EQR[n] < 0.625) {
-      data[n, "ALG_EQR_ADJ"] <-
-        ((data$ALG_EQR[n] / 0.625) * 0.2)
-    }
-  }
+  data$ALG_EQR_ADJ[data$ALG_EQR < 0.625] <-
+    (data$ALG_EQR[data$ALG_EQR < 0.625]
+    / 0.625) * 0.2
 
   # Combining the ecological ratios for each metric ---------------------------
-  for (n in 1:nrow(data)) {
-    if (data$Diversity_EQR_ADJ[n] < data$RMNI_EQR_ADJ[n]) {
-      data[n, "CompositionDiveristy"] <-
-        ((0.5 * data$Diversity_EQR_ADJ[n] + data$RMNI_EQR_ADJ[n])) / 1.5
-    } else {
-      data[n, "CompositionDiveristy"] <- data$RMNI_EQR_ADJ[n]
-    }
-  }
-  for (n in 1:nrow(data)) {
-    data[n, "Z"] <-
-      (2 *
-        (1 / (exp(log(2600000000) + data$REF_RMNI[n] * log(0.0166)) + 1 / 0.5)))
-  }
+  data$CompositionDiveristy <- data$RMNI_EQR_ADJ
+  data$CompositionDiveristy[data$Diversity_EQR_ADJ < data$RMNI_EQR_ADJ] <-
+    ((0.5 * data$Diversity_EQR_ADJ[data$Diversity_EQR_ADJ < data$RMNI_EQR_ADJ] +
+      data$RMNI_EQR_ADJ[data$Diversity_EQR_ADJ < data$RMNI_EQR_ADJ])) / 1.5
 
-  for (n in 1:nrow(data)) {
-    if (data$CompositionDiveristy[n] < data$ALG_EQR_ADJ[n]) {
-      data[n, "EQR_LEAFPACS"] <- data$CompositionDiveristy[n]
-    } else {
-      data[n, "EQR_LEAFPACS"] <-
-        (data$Z[n] * data$ALG_EQR_ADJ[n] +
-          data$CompositionDiveristy[n]) / (data$Z[n] + 1)
-    }
-  }
+  data$Z <-
+    (2 * (1 / (exp(log(2600000000) + data$REF_RMNI * log(0.0166)) + 1 / 0.5)))
+
+  data$EQR_LEAFPACS <-
+    (data$Z * data$ALG_EQR_ADJ + data$CompositionDiveristy) / (data$Z + 1)
+
+  data$EQR_LEAFPACS[data$CompositionDiveristy < data$ALG_EQR_ADJ] <-
+    data$CompositionDiveristy
 
   # Cap final EQR -------------------------------------------------------------
   data$EQR_LEAFPACS_Capped <- data$EQR_LEAFPACS
-  for (n in 1:nrow(data)) {
-    if (data$EQR_LEAFPACS[n] > 1) {
-      data[n, "EQR_LEAFPACS_Capped"] <- 1
-    }
-  }
+  data$EQR_LEAFPACS_Capped[data$EQR_LEAFPACS > 1] <- 1
 
   # Calculate CLASS ----------------------------------------------------------
-  for (n in 1:nrow(data)) {
-    if (data$EQR_LEAFPACS[n] < 0.2) {
-      data$CLASS[n] <- "BAD"
-    } else
-    if (data$EQR_LEAFPACS[n] < 0.4) {
-      data$CLASS[n] <- "POOR"
-    } else
-    if (data$EQR_LEAFPACS[n] < 0.6) {
-      data$CLASS[n] <- "MODERATE"
-    } else
-    if (data$EQR_LEAFPACS[n] < 0.8) {
-      data$CLASS[n] <- "GOOD"
-    } else
-    if (data$EQR_LEAFPACS[n] >= 0.8) {
-      data$CLASS[n] <- "HIGH"
-    }
-  }
+  data$CLASS[data$EQR_LEAFPACS < 0.2] <- "BAD"
+  data$CLASS[data$EQR_LEAFPACS < 0.4] <- "POOR"
+  data$CLASS[data$EQR_LEAFPACS < 0.6] <- "MODERATE"
+  data$CLASS[data$EQR_LEAFPACS < 0.8] <- "GOOD"
+  data$CLASS[data$EQR_LEAFPACS >= 0.8] <- "HIGH"
 
   return(data)
 }
--- tests/testthat/test-leafpacs-classify.R
+++ tests/testthat/test-leafpacs-classify.R
@@ -1,15 +1,15 @@
 test_that("leafpacs classifies", {
-   data <- get_demo_data()
-   output <- leafpacs_predict(data[1, ])
-   output <- leafpacs_classify(output)
+   demo_data <- get_demo_data()
+   data <- leafpacs_predict(demo_data[1, ])
+   data <- leafpacs_classify(data)
 
-  expect_equal(round(output$RMNI_EQR, 3), 0.632)
-  expect_equal(round(output$RMNI_EQR_ADJ, 3), 0.525)
-  expect_equal(round(output$NTAXA_EQR, 3), 1.134)
-  expect_equal(round(output$NFG_EQR, 3), 1.244)
-  expect_equal(round(output$ALG_EQR, 3), 0.516)
-  expect_equal(round(output$Diversity_EQR_ADJ, 3), 1.158)
-  expect_equal(round(output$ALG_EQR_ADJ, 3), 0.165)
+  expect_equal(round(data$RMNI_EQR, 3), 0.632)
+  expect_equal(round(data$RMNI_EQR_ADJ, 3), 0.525)
+  expect_equal(round(data$NTAXA_EQR, 3), 1.134)
+  expect_equal(round(data$NFG_EQR, 3), 1.244)
+  expect_equal(round(data$ALG_EQR, 3), 0.516)
+  expect_equal(round(data$Diversity_EQR_ADJ, 3), 1.158)
+  expect_equal(round(data$ALG_EQR_ADJ, 3), 0.165)
 
 
 })
